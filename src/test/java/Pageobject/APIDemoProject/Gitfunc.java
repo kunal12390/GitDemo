d5069cccde75844236c754c30c47419918754717
kunal <kunalm212@gmail.com>
2021-12-20 09:50:58 +0530

    This is commit from GitDemo guy

---
+++ src/test/java/Pageobject/APIDemoProject/Gitfunc.java
@@ -0,0 +1,19 @@
+package Pageobject.APIDemoProject;
+
+public class Gitfunc {
+	
+	
+	public void getEat() {
+		
+		System.out.println("Kartik is eating");
+	}
+
+	public static void main(String[] args) {
+		
+		Gitfunc nf = new Gitfunc();
+		nf.getEat();
+		
+		
+	}
+
+}
